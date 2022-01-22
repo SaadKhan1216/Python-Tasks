6302d4c4899d0064ed5294cad4bedd81ddbcfcbc
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-22 20:39:20 +0500

    Loop

---
+++ LoopElse.py
@@ -0,0 +1,13 @@
+# While Else BRANCH........
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
+
+# For Else BRANCH.........
+for j in range(5):
+    print(j)
+else:
+    print("else:", j)
